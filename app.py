2ad1241c3ddc442e0e34ee45de3ee1142799f501
Alexis Rodriguez <ralexis1092@gmail.com>
2024-03-19 15:04:33 -0600

    Added query string and add API

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! This is a test!'
+
+
+@app.route('/parameters', methods=['GET'])
+def query_strings():
+
+    args1 = request.args['a']
+    args2 = request.args['b']
+    args3 = request.args['c']
+
+    return '''<h1>The Query String are...{}:{}:{}</h1>''' .format(args1,args2,args3)
+
+
+@app.route('/add', methods=['GET'])
+def query_strings():
+
+    args1 = request.args['a']
+    args2 = request.args['b']
+
+    return '''<h1>The Sum is {}</h1>''' .format(sum(args1,args2))
